bd4fc31221ad4fba4beeb0532cf9125c2dbb642f
Oleg Novitsky <darkmus@tut.by>
2019-08-28 16:32:35 +0300

    Create a class MenuItem

---
+++ src/components/menu-item.js
@@ -0,0 +1,19 @@
+import {createElement, toCapitalize} from './utils.js';
+
+export class MenuItem {
+  constructor(menuItem) {
+    this._name = menuItem.name;
+    this._isActive = menuItem.isActive;
+    this._element = null;
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+  }
+
+  getTemplate() {
+    return `<a class="trip-tabs__btn${this._isActive ? ` trip-tabs__btn--active` : ``}" href="#">${toCapitalize(this._name)}</a>`;
+  }
+}
