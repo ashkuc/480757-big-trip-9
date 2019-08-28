a87a8c2e4bffa13a690863d082e6971bcae4250b
Oleg Novitsky <darkmus@tut.by>
2019-08-28 15:58:37 +0300

    Add helper functions in the utils.js

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,4 +1,4 @@
-const shuffleArray = (array) => {
+export const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [array[i]] = [array[j]];
@@ -7,16 +7,42 @@ const shuffleArray = (array) => {
   return array;
 };
 
-const toTimeForEdit = (miliseconds) => {
+export const toTimeForEdit = (miliseconds) => {
   const dateTime = new Date(miliseconds).toLocaleString();
   let date = dateTime.split(/\.|,/).slice(0, 3).map((element) => element.slice(0, 2)).join(`/`);
   let time = dateTime.split(/\.| |,|:/).slice(4, 6).join(`:`);
   return `${date} ${time}`;
 };
 
-const getRandomBoolean = () => Boolean(Math.round(Math.random()));
-const getRandomBetween = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
-const getRandomFromArray = (array) => array[getRandomBetween(0, array.length - 1)];
-const toCapitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+export const getRandomBoolean = () => Boolean(Math.round(Math.random()));
+export const getRandomBetween = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+export const getRandomFromArray = (array) => array[getRandomBetween(0, array.length - 1)];
+export const toCapitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
-export {shuffleArray, getRandomBoolean, getRandomBetween, toCapitalize, toTimeForEdit, getRandomFromArray};
+export const Position = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+  return newElement.firstChild;
+};
+
+export const render = (container, element, position) => {
+  switch (position) {
+    case Position.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case Position.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+export const unrender = (element) => {
+  if (element) {
+    element.remove();
+  }
+};
