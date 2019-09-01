0b6a0fd82905548a28ac755d154681afd90f37f0
Darkmus <darkmus@tut.by>
2019-09-01 13:59:47 +0300

    Create a new class TripController & move events render logic to it

---
+++ src/components/trip-controller.js
@@ -0,0 +1,99 @@
+import {Position, render} from './utils.js';
+import {getSortItems} from '../sample-data.js';
+import {DaysList} from './days-list.js';
+import {Day} from './day.js';
+import {DayInfo} from './day-info.js';
+import {EventsList} from './events-list.js';
+import {Event} from './event.js';
+import {EventForm} from './event-form.js';
+import {SortContainer} from './sort-container.js';
+import {SortItem} from './sort-item.js';
+import {NoEvents} from './no-events.js';
+
+export default class {
+  constructor(container, events) {
+    this._container = container;
+    this._events = events;
+    this._daysList = new DaysList();
+    this._day = new Day();
+    this._dayInfo = new DayInfo();
+    this._eventsList = new EventsList();
+    this._sortItems = getSortItems();
+    this._sortContainer = new SortContainer();
+    this._noEvents = new NoEvents();
+  }
+
+  _renderSortItem(sortItemInfo) {
+    const sortItem = new SortItem(sortItemInfo);
+    render(this._sortContainer.getElement().querySelector(`span:last-child`), sortItem.getElement(), Position.BEFORE);
+  }
+
+  _renderSort() {
+    this._sortItems.forEach((sortItemInfo) => this._renderSortItem(sortItemInfo));
+    render(this._container.querySelector(`h2`), this._sortContainer.getElement(), Position.AFTER);
+  }
+
+  _renderEvent(eventInfo) {
+    const event = new Event(eventInfo);
+    const eventForm = new EventForm(eventInfo);
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === `Escape` || evt.key === `Esc`) {
+        this._eventsList.getElement().replaceChild(event.getElement(), eventForm.getElement());
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+
+    event.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+      this._eventsList.getElement().replaceChild(eventForm.getElement(), event.getElement());
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+    eventForm.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+      this._eventsList.getElement().replaceChild(event.getElement(), eventForm.getElement());
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+    eventForm.getElement().querySelectorAll(`input[type="text"]`).forEach((input) => {
+      input.addEventListener(`focus`, () => {
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      });
+    });
+
+    eventForm.getElement().querySelectorAll(`input[type="text"]`).forEach((input) => {
+      input.addEventListener(`blur`, () => {
+        document.addEventListener(`keydown`, onEscKeyDown);
+      });
+    });
+
+    eventForm.getElement().querySelector(`form`).addEventListener(`submit`, () => {
+      this._eventsList.getElement().replaceChild(event.getElement(), eventForm.getElement());
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+    render(this._eventsList.getElement(), event.getElement(), Position.BEFOREEND);
+  }
+
+  _renderEvents() {
+    this._events.forEach((eventInfo) => this._renderEvent(eventInfo));
+    render(this._day.getElement(), this._eventsList.getElement(), Position.BEFOREEND);
+  }
+
+  _renderMainContent() {
+    if (this._events.length === 0) {
+      render(this._container, this._noEvents.getElement(), Position.BEFOREEND);
+    } else {
+      this._renderSort();
+      render(this._container, this._daysList.getElement(), Position.BEFOREEND);
+      render(this._daysList.getElement(), this._day.getElement(), Position.BEFOREEND);
+      render(this._day.getElement(), this._dayInfo.getElement(), Position.BEFOREEND);
+      this._renderEvents();
+    }
+  }
+
+  init() {
+    this._renderMainContent();
+  }
+}
